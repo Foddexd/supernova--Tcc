c0a1f7815179349d045c411d9ccdd83ad679c019
SilvioMartinezz <slsmartinez20@gmail.com>
2025-04-14 23:55:58 -0300

    .

---
+++ Supernova/Assets/Script/AtivaChefe.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AtivadorPorTrigger : MonoBehaviour
+{
+    [Header("Objeto a ser ativado")]
+    public GameObject objetoParaAtivar;
+
+    [Header("Código a ser executado")]
+    public UnityEvent aoAtivar;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (objetoParaAtivar != null)
+                objetoParaAtivar.SetActive(true);
+
+            aoAtivar.Invoke();
+        }
+    }
+}
