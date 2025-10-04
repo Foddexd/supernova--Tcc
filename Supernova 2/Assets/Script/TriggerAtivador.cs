d3e006ddf1a7294d62ddf862f57a1735d1acf25a
Foddexd <augusto.germano2013@gmail.com>
2025-10-04 10:29:15 -0300

    Ato 1 Desativa e Ato 2 ativa

--- Supernova 2/Assets/Script/TriggerAtivador.cs
+++ Supernova 2/Assets/Script/TriggerAtivador.cs
@@ -6,6 +6,8 @@ public class TriggerActivator : MonoBehaviour
     public GameObject item2;
     public GameObject item3;
     public GameObject item4;
+    public GameObject item5;
+    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,6 +17,9 @@ public class TriggerActivator : MonoBehaviour
             if (item2 != null) item2.SetActive(true);
             if (item3 != null) item3.SetActive(false);
             if (item4 != null) item4.SetActive(false);
+            if (item5 != null) item5.SetActive(true);
+          
+
         }
     }
 }
